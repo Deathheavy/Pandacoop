91f765af6542809c4622fe66377753b05c713e9f
Deathheavy <164697563+Deathheavy@users.noreply.github.com>
2025-07-15 16:04:21 -0300

    Update sobreCommand.js

--- utils/sobreCommand.js
+++ utils/sobreCommand.js
@@ -4,16 +4,16 @@ async function handleSobreCommand(msg) {
   const botAvatar = msg.client?.user?.displayAvatarURL?.() || null;
   
   const embed = new EmbedBuilder()
-    .setTitle('<a:about:1378756920063692883> Sobre o Pandacoop')
+    .setTitle('Sobre o BOT')
     .setColor(0xae6800)
     .setDescription(`Este bot foi criado para gerenciar jogos de forma prática e facilitar o compartilhamento e organização no servidor`)
     .addFields(
       {
-        name: '<:Sailors:1377016721139040296> Fontes',
+        name: 'Fontes',
         value: `• [**Online-Fix**](https://online-fix.me)\n• [**Fitgirl**](https://fitgirl-repacks.site)\n• [**DODI**](https://dodi-repacks.site)`,
       },
       {
-        name: '<:recursos:1377008255683006586> Recursos',
+        name: 'Recursos',
         value: `• Node.js\n• Discord.js\n• Steam Web API\n• Youtube API\n• Cache em memória (Map)`,
       },
       {
